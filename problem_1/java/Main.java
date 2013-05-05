c0a0ae1eaa4ab231affaed86fff7ced04808450a
swafford <johnmarksuave@gmail.com>
2013-05-04 22:26:01 -0500

    Problem 1 finished in Java

---
+++ problem_1/java/Main.java
@@ -0,0 +1,16 @@
+public class Main{
+
+    public static void main(String args[]){
+
+	int max = 1000;
+	int sum = 0;
+
+	for(int i = 3; i < max; i++)
+	    if(i % 3 == 0 || i % 5 == 0)
+		sum += i;
+
+	System.out.println(sum);
+
+    }
+
+}
